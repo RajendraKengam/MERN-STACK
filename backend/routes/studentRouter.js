221465009f2a97d49d520414a85d99ced7e5553f
RajendraKengam <rajendrakengam270@gmail.com>
2026-05-15 09:38:33 +0530

    Day-21 of class

--- backend/routes/studentRouter.js
+++
@@ -1,8 +0,0 @@
-const addStudent = require("../controller/studentController");
-const express = require("express");
-const router = express.Router();
-router.post("/add-student", addStudent);
-
-
-
-module.exports = router;
